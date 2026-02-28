a911a579aabde31a9658855e743cd90f0321a3cf
Aarti <aratibokade123@gmail.com>
2026-02-28 16:43:58 +0530

    Product class Impl

--- src/com/smartshop/entity/Product.java
+++ src/com/smartshop/entity/Product.java
@@ -1,5 +1,71 @@
 package com.smartshop.entity;
 
 public class Product {
+	// Fields required for Product class
+    private int productId;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+
+    // Default Constructor
+    public Product() {}
+
+    // Parameterized Constructor 
+    public Product(int productId, String name, String description, double price, int quantity) {
+        this.productId = productId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Getters and Setters 
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", name=" + name + ", description=" + description + ", price="
+				+ price + ", quantity=" + quantity + "]";
+	}
+
 
 }
